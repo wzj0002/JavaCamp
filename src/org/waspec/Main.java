bb635507f37e3393b42d70aa99412340580a492e
wzj0002 <wenchan.jiang@gmail.com>
2015-02-11 10:33:27 -0500

    99 multiplication table

---
+++ src/org/waspec/Main.java
@@ -0,0 +1,14 @@
+package org.waspec;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.printf("%d * %d = %d\t", j, i, j * i);
+            }
+            System.out.println();
+        }
+
+    }
+}
